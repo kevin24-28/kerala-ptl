0eb41d1fadbe8f6c2a18df3a126cfea84c4d04dc
kevin24-28 <kevin@keon.in>
2026-08-19 15:52:46 +0530

    Add barcode generation function using bwip-js

--- labelService.js
+++ labelService.js
@@ -1,1 +1,19 @@
-labelService.js
+const bwipjs = require('bwip-js');
+
+async function generateBarcodeBase64(text) {
+  return new Promise((resolve, reject) => {
+    bwipjs.toBuffer({
+      bcid: 'code128',
+      text: text,
+      scale: 3,
+      height: 10,
+      includetext: true,
+      textxalign: 'center',
+    }, (err, png) => {
+      if (err) reject(err);
+      else resolve(`data:image/png;base64,${png.toString('base64')}`);
+    });
+  });
+}
+
+module.exports = { generateBarcodeBase64 };
